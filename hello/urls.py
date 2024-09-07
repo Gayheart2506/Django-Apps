a0c0ec1f82ebb971d9b7a5dc0c2b72bc902043fd
Gayheart2506 <107317418+Gayheart2506@users.noreply.github.com>
2024-09-07 01:11:55 +0000

    update

--- hello/urls.py
+++ hello/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"), 
+    # path("", views.index, name="index"), 
     path("<str:name>", views.greet, name="greet"),
     path("gayheart", views.gayheart, name="gayheart"),
     path("jessie", views.jessie, name="jessie")
